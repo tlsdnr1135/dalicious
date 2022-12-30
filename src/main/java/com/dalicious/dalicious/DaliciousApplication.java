3d016d0ab3212cacb18bcaaecc357d416cc84185
sulbin <tlsdnr1135@naver.com>
2022-12-31 05:01:17 +0900

    "기본 설정"

---
+++ src/main/java/com/dalicious/dalicious/DaliciousApplication.java
@@ -0,0 +1,14 @@
+package com.dalicious.dalicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaliciousApplication {
+
+	public static void main(String[] args) {
+		System.out.println("테스트");
+		SpringApplication.run(DaliciousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalicious/dalicious/DaliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalicious.dalicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaliciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
